9742cab5b13615f189c10e389c2ed0d1e78ff524
JohnLesterDev <johnlesterincbusiness@gmail.com>
2025-08-21 08:54:03 +0800

    Homepage Initialization

---
+++ res/scripts/home.js
@@ -0,0 +1,13 @@
+const mnCont = document.querySelector('.home-container');
+const logo = document.querySelector('.logo');
+const mainNavLinks = document.querySelectorAll('.main-nav ul li a');
+const modal = document.querySelector('.random-passage-modal');
+
+logo.addEventListener('click', () => {
+    mnCont.classList.toggle('active');
+    modal.classList.toggle('visible');
+    modal.classList.toggle('hidden');
+
+    logo.classList.toggle('active');
+    mainNavLinks.forEach(link => link.classList.toggle('active'));
+});
